ebf4aa308964570be2339b2f5a6af3746a81f39d
Abhay Singh <abhaybsingh19w33@gmail.com>
2021-12-06 15:33:41 +0530

    Drawer

--- awesome_app/lib/main.dart
+++ awesome_app/lib/main.dart
@@ -26,54 +26,76 @@ class HomePage extends StatelessWidget {
       ),
       // body: const Text("Revision course restart"),
       // aligning the container in center
-      body: Container(
-        color: Colors.teal,
-        // height: 500,
-        width: 500,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              // this makes whole container color red
-              // cant use this property here while using boxDecoration
-              color: Colors.red,
-              // creating a box
-              width: 100,
-              height: 100,
-              // this is like handling things while cant be contained in specified area
-              // clipBehavior: Clip.none,
-              alignment: Alignment.center,
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          // this makes whole container color red
+          // cant use this property here while using boxDecoration
+          color: Colors.orange,
+          // creating a box
+          width: 100,
+          height: 100,
+          // this is like handling things while cant be contained in specified area
+          // clipBehavior: Clip.none,
+          alignment: Alignment.center,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: const EdgeInsets.all(0),
+          children: const <Widget>[
+            // DrawerHeader(
+            //   child: Text("Menu Drawer"),
+            //   decoration: BoxDecoration(color: Colors.orange),
+            // ),
+            // for more design use this
+            UserAccountsDrawerHeader(
+              accountName: Text("Abhay Singh"),
+              accountEmail: Text("abhaybsingh19w33@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://images.unsplash.com/photo-1562040506-a9b32cb51b94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
+              ),
+
+              // normal image displayed
+              // currentAccountPicture: Image(
+              //     image: NetworkImage(
+              //         "https://images.unsplash.com/photo-1562040506-a9b32cb51b94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80")),
+
+              // this haas some const error
+              // currentAccountPicture: Image.network(
+              //     "https://images.unsplash.com/photo-1562040506-a9b32cb51b94?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              // this makes whole container color red
-              // cant use this property here while using boxDecoration
-              color: Colors.amber,
-              // creating a box
-              width: 100,
-              height: 100,
-              // this is like handling things while cant be contained in specified area
-              // clipBehavior: Clip.none,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text("Abhay Singh"),
+              subtitle: Text("Developer"),
+              trailing: Icon(Icons.edit),
+              // for ripple effect on tap like a button
+              // onTap: (){},
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              // this makes whole container color red
-              // cant use this property here while using boxDecoration
-              color: Colors.blue,
-              // creating a box
-              width: 100,
-              height: 100,
-              // this is like handling things while cant be contained in specified area
-              // clipBehavior: Clip.none,
-              alignment: Alignment.center,
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Email"),
+              subtitle: Text("abhaybsingh19w33@gmail.com"),
+              trailing: Icon(Icons.edit),
             ),
+            // ListTile(
+            //   leading: Icon(Icons.person),
+            //   title: Text("Abhay Singh"),
+            //   subtitle: Text("Developer"),
+            //   trailing: Icon(Icons.edit),
+            // )
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          return;
+        },
+        child: const Icon(Icons.edit),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
     // return Container(
     //   color: Colors.amber,
